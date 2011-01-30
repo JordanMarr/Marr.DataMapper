c1f6e94b127822758ad43358c30ba3cb1a6954e4
jmarr_cp <SND\jmarr_cp>
2011-01-29 16:58:02 -0800

    Branched from $/MarrDataMapper/trunk

---
+++ branches/v1.0.1/Marr.Data/Mapping/ColumnMap.cs
@@ -0,0 +1,41 @@
+/*  Copyright (C) 2008 - 2011 Jordan Marr
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library. If not, see <http://www.gnu.org/licenses/>. */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace Marr.Data.Mapping
+{
+    /// <summary>
+    /// Contains information about the class fields and their associated stored proc parameters
+    /// </summary>
+    public class ColumnMap
+    {
+        public ColumnMap(string fieldName, Type fieldType, Enum dbType, IColumnInfo columnAttribute)
+        {
+            FieldName = fieldName;
+            FieldType = fieldType;
+            DBType = dbType;
+            ColumnInfo = columnAttribute;
+        }
+
+        public string FieldName { get; set; }
+        public Type FieldType { get; set; }
+        public Enum DBType { get; set; }
+        public IColumnInfo ColumnInfo { get; set; }
+    }
+}
---
+++ branches/v1.0.1/Marr.Data/Mapping/Relationship.cs
@@ -0,0 +1,58 @@
+/*  Copyright (C) 2008 - 2011 Jordan Marr
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library. If not, see <http://www.gnu.org/licenses/>. */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace Marr.Data.Mapping
+{
+    public class Relationship
+    {
+        private IRelationshipInfo _relationshipInfo;
+        private MemberInfo _member;
+
+        public Relationship(IRelationshipInfo relationshipInfo, MemberInfo member)
+        {
+            _relationshipInfo = relationshipInfo;
+            _member = member;
+        }
+
+        public IRelationshipInfo RelationshipInfo
+        {
+            get { return _relationshipInfo; }
+        }
+
+        public MemberInfo Member
+        {
+            get { return _member; }
+        }
+
+        public Type MemberType
+        {
+            get
+            {
+                // Assumes that a relationship can only have a member type of Field or Property
+
+                if (Member.MemberType == MemberTypes.Field)
+                    return (Member as FieldInfo).FieldType;
+                else
+                    return (Member as PropertyInfo).PropertyType;
+            }
+        }
+
+    }
+}
