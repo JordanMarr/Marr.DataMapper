98272ff8c5d6e58c6c15d66384a69397c3fac17f
jmarr_cp <SND\jmarr_cp>
2011-02-10 19:13:37 -0800

    v2.3

--- trunk/Marr.Data/Properties/AssemblyInfo.cs
+++ trunk/Marr.Data/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.*")]
-[assembly: AssemblyInformationalVersion("2.2 beta")]
+[assembly: AssemblyVersion("2.3.*")]
+[assembly: AssemblyInformationalVersion("2.3 beta")]
