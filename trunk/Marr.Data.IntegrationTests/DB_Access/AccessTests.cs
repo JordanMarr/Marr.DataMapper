bafab4bd8a3284bf06a801d7a7035522920a6245
jmarr_cp <SND\jmarr_cp>
2011-04-09 08:53:37 -0700

    Added another Access timed test.

--- trunk/Marr.Data.IntegrationTests/DB_Access/AccessTests.cs
+++ trunk/Marr.Data.IntegrationTests/DB_Access/AccessTests.cs
@@ -27,7 +27,7 @@ namespace Marr.Data.IntegrationTests.DB_Access
         ///                 -> List of Option (1-M)
         /// </summary>
         [TestMethod]
-        public void TimedTest()
+        public void TimedTest_AccessQuery()
         {
             var times = new List<long>();
             var profiler = new Stopwatch();
@@ -42,13 +42,51 @@ namespace Marr.Data.IntegrationTests.DB_Access
 
                     profiler.Stop();
 
+                    if (products.Count == 0)
+                        throw new Exception("Result set was empty");
+
                     Trace.WriteLine(string.Format("iteration {0} : {1}ms", i, profiler.ElapsedMilliseconds));
                     times.Add(profiler.ElapsedMilliseconds);
                 }
             }
 
             Trace.WriteLine("---------------");
-            Trace.WriteLine(string.Format("Average time: {0}", times.Average()));
+            times.RemoveAt(0);
+            Trace.WriteLine(string.Format("Average time after inital load: {0}", times.Average()));
         }
+
+        [TestMethod]
+        public void TimedTest_PassingInQueryText()
+        {
+            var times = new List<long>();
+            var profiler = new Stopwatch();
+
+            using (var db = CreateAccessDB())
+            {
+                db.SqlMode = SqlModes.Text;
+
+                for (int i = 1; i <= 4; i++)
+                {
+                    profiler.Restart();
+
+                    string sql = @"SELECT Product.ID, Product.Name, Product.Description, Product.Price, Product.CategoryID, Product.ImageFileName, Product.NewItem, Product.IsSplash, Category.Name AS CategoryName, Option.OptionTypeID, OptionType.Type, OptionType.MultiPick, Option.ID AS OptionID, Option.Description AS OptionDescription, Option.Price AS OptionPrice
+                                   FROM ((Category LEFT JOIN OptionType ON Category.ID = OptionType.CategoryID) RIGHT JOIN Product ON Category.ID = Product.CategoryID) LEFT JOIN [Option] ON OptionType.ID = Option.OptionTypeID;";
+                    var products = db.Query<Product>().QueryText(sql).Graph().ToList();
+
+                    profiler.Stop();
+
+                    if (products.Count == 0)
+                        throw new Exception("Result set was empty");
+
+                    Trace.WriteLine(string.Format("iteration {0} : {1}ms", i, profiler.ElapsedMilliseconds));
+                    times.Add(profiler.ElapsedMilliseconds);
+                }
+            }
+
+            Trace.WriteLine("---------------");
+            times.RemoveAt(0);
+            Trace.WriteLine(string.Format("Average time after inital load: {0}", times.Average()));
+        }
+
     }
 }
