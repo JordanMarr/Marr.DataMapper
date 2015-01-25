437360603c11c03d3febfaec6116726ff1cd6177
Jordan Marr <jnmarr AT hotmail.com>
2015-01-25 02:46:02 -0500

    Updated to v3.30.

--- Marr.Data/Properties/AssemblyInfo.cs
+++ Marr.Data/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Marr.Data")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright ©  2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.22.*")]
-[assembly: AssemblyInformationalVersion("3.22")]
+[assembly: AssemblyVersion("3.30.*")]
+[assembly: AssemblyInformationalVersion("3.30")]
