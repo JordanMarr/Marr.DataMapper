35307558ccf85f8d3a9200d42d04b87a99d9607e
Jordan Marr <jnmarr AT hotmail.com>
2015-01-29 00:34:01 -0500

    Updated to v3.34

--- Marr.Data/Properties/AssemblyInfo.cs
+++ Marr.Data/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.33.*")]
-[assembly: AssemblyInformationalVersion("3.33")]
+[assembly: AssemblyVersion("3.34.*")]
+[assembly: AssemblyInformationalVersion("3.34")]
