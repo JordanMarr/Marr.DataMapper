3010221d292715b60ad378764659687010373277
Jordan Marr <jnmarr@hotmail.com>
2015-03-06 00:26:29 -0500

    Added unit tests to ensure that two of the same entity type can exist as members at the same level of an object graph, unaffected by type or identity values.

--- Marr.Data.UnitTests/SameEntityTests.cs
+++ Marr.Data.UnitTests/SameEntityTests.cs
@@ -28,9 +28,12 @@ namespace Marr.Data.UnitTests
 		{
 			public int ID { get; set; }
 			public string Name { get; set; }
+
+			public int ChildID { get; set; }
+			public DupedClassChild Child { get; set; }
 		}
 
-		public class AnotherClass
+		public class DupedClassChild
 		{
 			public int ID { get; set; }
 			public string Name { get; set; }
@@ -42,8 +45,8 @@ namespace Marr.Data.UnitTests
 		public void Init()
 		{
 			FluentMappings mappings = new FluentMappings();
-            mappings
-                .Entity<RootClass>()
+			mappings
+				.Entity<RootClass>()
 					.Columns.AutoMapSimpleTypeProperties()
 						.For(r => r.ID).SetPrimaryKey()
 					.Relationships.MapProperties()
@@ -51,23 +54,32 @@ namespace Marr.Data.UnitTests
 							.EagerLoad((db, r) => db.Query<DupedClass>().Where(d => d.ID == r.Dupe1ID).FirstOrDefault())
 						.For(r => r.Dupe2)
 							.EagerLoad((db, r) => db.Query<DupedClass>().Where(d => d.ID == r.Dupe2ID).FirstOrDefault())
-                .Entity<DupedClass>()
+
+				.Entity<DupedClass>()
 					.Columns.AutoMapSimpleTypeProperties()
+						.For(d => d.ID).SetPrimaryKey()
+					.Relationships.MapProperties()
+						.For(d => d.Child)
+							.JoinOne(d => d.Child, (d, dcc) => d.ChildID == dcc.ID)
+
+				.Entity<DupedClassChild>()
+					.Columns.AutoMapSimpleTypeProperties()
+						.PrefixAltNames("dcc")
 						.For(d => d.ID).SetPrimaryKey();
 		}
 
 		[TestMethod]
-		public void DupesWithDifferentIDs_NoProblemsExpected()
+		public void DupesAtSameLevel_WithDifferentIDs()
 		{
 			// Arrange
 			var rs1 = new StubResultSet("ID", "Dupe1ID", "Dupe2ID");
             rs1.AddRow(1, 100, 101);
 
-			var rs2 = new StubResultSet("ID", "Name");
-			rs2.AddRow(100, "Dupe_100");
+			var rs2 = new StubResultSet("ID", "Name", "ChildID");
+			rs2.AddRow(100, "Dupe_100", 1000);
 
-			var rs3 = new StubResultSet("ID", "Name");
-			rs3.AddRow(101, "Dupe_101");
+			var rs3 = new StubResultSet("ID", "Name", "ChildID");
+			rs3.AddRow(101, "Dupe_101", 1001);
 
             var db = CreateDB_ForQuery(rs1, rs2, rs3);
 
@@ -85,6 +97,77 @@ namespace Marr.Data.UnitTests
 
 			Assert.AreEqual(100, rootEnt.Dupe1.ID);
 			Assert.AreEqual(101, rootEnt.Dupe2.ID);
+		}
+
+		[TestMethod]
+		public void DupesAtSameLevel_WithSameIDs()
+		{
+			// Arrange
+			var rs1 = new StubResultSet("ID", "Dupe1ID", "Dupe2ID");
+			rs1.AddRow(1, 100, 100);
+
+			var rs2 = new StubResultSet("ID", "Name", "ChildID");
+			rs2.AddRow(100, "Dupe_100", 1000);
+
+			var rs3 = new StubResultSet("ID", "Name", "ChildID");
+			rs3.AddRow(100, "Dupe_100", 1000);
+
+			var db = CreateDB_ForQuery(rs1, rs2, rs3);
+
+			// Act
+			var rootEnt = db.Query<RootClass>()
+				.Graph(r => r.Dupe1, r => r.Dupe2)
+				.FirstOrDefault();
+
+			// Assert
+			Assert.AreEqual(100, rootEnt.Dupe1ID);
+			Assert.AreEqual(100, rootEnt.Dupe2ID);
+
+			Assert.IsNotNull(rootEnt.Dupe1);
+			Assert.IsNotNull(rootEnt.Dupe2);
+
+			Assert.AreEqual(100, rootEnt.Dupe1.ID);
+			Assert.AreEqual(100, rootEnt.Dupe2.ID);
+		}
+
+		[TestMethod]
+		public void DupesAtSameLevel_WithSameIDs_OneWithJoin()
+		{
+			// Arrange
+			var rsRoot = new StubResultSet("ID", "Dupe1ID", "Dupe2ID");
+			rsRoot.AddRow(1, 100, 100);
+
+			var rsDupe1 = new StubResultSet("ID", "Name", "ChildID", "dccID", "dccName");
+			rsDupe1.AddRow(100, "Dupe_100", 1000, 1000, "Child_1000");
+
+			var rsDupe2 = new StubResultSet("ID", "Name", "ChildID");
+			rsDupe2.AddRow(100, "Dupe_100", 1000);
+
+			var db = CreateDB_ForQuery(rsRoot, rsDupe1, rsDupe2);
+
+			// Act
+			var rootEnt = db.Query<RootClass>()
+				.Graph(
+					r => r.Dupe1,
+					r => r.Dupe2,
+					r => r.Dupe1.Child)
+				.FirstOrDefault();
+
+			// Assert
+			Assert.AreEqual(100, rootEnt.Dupe1ID);
+			Assert.AreEqual(100, rootEnt.Dupe2ID);
+
+			Assert.IsNotNull(rootEnt.Dupe1);
+			Assert.IsNotNull(rootEnt.Dupe2);
+
+			Assert.AreEqual(100, rootEnt.Dupe1.ID);
+			Assert.AreEqual(100, rootEnt.Dupe2.ID);
+
+			Assert.IsNotNull(rootEnt.Dupe1.Child);
+			Assert.IsNull(rootEnt.Dupe2.Child);
+
+			Assert.AreEqual(1000, rootEnt.Dupe1.Child.ID);
+			Assert.AreEqual("Child_1000", rootEnt.Dupe1.Child.Name);
 		}
 	}
 }
